55bc4c7eaed14c015bf4085c3ae870d8bcbdc8a5
sheema <sheema.n@greatinnovus.com>
2019-01-06 15:40:09 +0530

    Comment box Set up for forum screens

--- mobileapp/src/pages/notification/notification.ts
+++ mobileapp/src/pages/notification/notification.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpProvider } from '../../providers/http/http';
 import { API_URL } from '../../constants/API_URLS.var';
 import { Constant } from '../../constants/Constant.var';
+import { LoaderService } from '../../service/loaderService';
 
 @IonicPage({
   name: 'notification-page'
@@ -13,19 +14,24 @@ import { Constant } from '../../constants/Constant.var';
 })
 export class NotificationPage implements OnInit {
   notificationList: any = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpProvider, public API_URL: API_URL,public constant:Constant) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpProvider, public API_URL: API_URL,public constant:Constant, private loader: LoaderService) {
+  }
+  ionViewDidEnter() {
+    this.getNotification();
   }
   ngOnInit(){
-    this.getNotification();
+   
   }
   ionViewDidLoad() { 
     console.log('ionViewDidLoad NotificationPage');
   }
   getNotification() {
+    this.loader.showLoader();
     this.http.getData(API_URL.URLS.getNotification).subscribe((data) => {
       if (data['isSuccess']) {
         this.notificationList = data['NotificationList'];
       }
+      this.loader.hideLoader();
       console.log(this.notificationList);
     });
   }
