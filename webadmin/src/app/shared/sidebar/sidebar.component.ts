9c2e295a151fc434b865a48fc4940617fe2503ef
GisBooshna <booshna.p@greatinnovus.com>
2019-03-29 12:55:00 +0530

    side bar change

--- webadmin/src/app/shared/sidebar/sidebar.component.ts
+++ webadmin/src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UtilService  } from '../../services/util.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
 export class SideBarComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, private utilservice: UtilService) { }
+   
+   role;
+   peerAdmin;
+   networkAdmin;
 
   ngOnInit() {
+       let role = this.utilservice.getRole();
+       if(role == 1){
+         this.networkAdmin = true;
+         this.peerAdmin = false;
+       }else{
+         this.peerAdmin = true;
+         this.networkAdmin =  false;
+       }
   }
 
 }
