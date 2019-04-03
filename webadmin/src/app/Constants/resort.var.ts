24a8f33ee975b9ccd043d97e16046e569994126c
gisvignesh <vignesh.k@greatinnovus.com>
2019-04-03 18:06:05 +0530

    resort management add

---
+++ webadmin/src/app/Constants/resort.var.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ResortVar {
+    url;
+    title = "Add New Resort";
+    resortList;
+    resortName;
+    location;
+    email;
+    mobile;
+    utilizedSpace;
+    numberOfUser;
+    status;
+    storageSpace;
+    allocatedSpace;
+    labels ={
+        resortName : "Resort Name",
+        location : "Location",
+        email : "Email",
+        mobile : "Mobile",
+        utilizedSpace : "Utilized Space",
+        numberOfUser : "No. of Users",
+        status : "Status",
+        createResort : "Add New",
+        division : "Division",
+        department : "Department"
+    };
+    divisionTemplate: any[] = [{
+        division: 1,
+        divisionName : 'Division1'
+    }];
+    departmentTemplate: any[] = [{
+        department: 1,
+        departmentName : ''
+    }];
+}
