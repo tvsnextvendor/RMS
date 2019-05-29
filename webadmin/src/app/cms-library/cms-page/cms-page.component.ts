c4e187dea673a23d24463e6b0516836474be22fa
gisvignesh <vignesh.k@greatinnovus.com>
2019-05-29 17:12:09 +0530

    Sidebar edit tab update

--- webadmin/src/app/cms-library/cms-page/cms-page.component.ts
+++ webadmin/src/app/cms-library/cms-page/cms-page.component.ts
@@ -9,17 +9,21 @@ import { CommonLabels } from '../../Constants/common-labels.var'
   styleUrls: ['./cms-page.component.css']
 })
 export class CmsPageComponent implements OnInit {
-
+  urlType;
   constructor(private route: Router,private activatedRoute: ActivatedRoute,private headerService :HeaderService,public commonLabels : CommonLabels) { }
 
   ngOnInit() {
     this.headerService.setTitle({title:this.commonLabels.titles.cmsLibrary, hidemodule:false});
+    this.activatedRoute.queryParams.subscribe(params=>{
+      console.log(params)
+      this.urlType = params.type;
+    })
   }
 
   pageRedirection(type,data){
     // this.activeType = type+data;
     // console.log(this.activeType)
-    if(data == 'quiz'){
+    if(type == 'create' && data == 'quiz'){
       this.route.navigate(['/createQuiz'])
     }
     else{
