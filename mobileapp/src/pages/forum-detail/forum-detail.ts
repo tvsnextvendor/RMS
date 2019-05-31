96f13eec7a6d34aa8cc32bcbda012d89ff00c7ce
sheema <sheema.n@greatinnovus.com>
2019-05-31 16:34:33 +0530

    commit code update for success messages

--- mobileapp/src/pages/forum-detail/forum-detail.ts
+++ mobileapp/src/pages/forum-detail/forum-detail.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, TemplateRef,ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController,Content } from 'ionic-angular';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController, Content } from 'ionic-angular';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -9,7 +9,7 @@ import { API_URL } from '../../constants/API_URLS.var';
 import { HttpProvider } from '../../providers/http/http';
 import { Storage } from '@ionic/storage';
 import { ModalPage } from '../modal/modal';
-import { LoaderService} from '../../service';
+import { LoaderService } from '../../service';
 import * as moment from 'moment';
 
 
@@ -36,7 +36,7 @@ export class ForumDetailPage implements OnInit {
   forumId;
   editComment;
   commentId;
-  editDisable=true;
+  editDisable = true;
   forumFavor: any = [];
   forumRecent: any = [];
   forumFeature: any = [];
@@ -52,11 +52,15 @@ export class ForumDetailPage implements OnInit {
   description;
   postId;
   status;
-  isComment=false;
-  isAnswer=false;
-  isActive=false;
-  hideQuestionBtn:boolean = true;
-  constructor(public navCtrl: NavController,public loader: LoaderService,public navParams: NavParams, private modalService: BsModalService, public constant: Constant, private toastr: ToastrService, public API_URL: API_URL, private http: HttpProvider, private storage: Storage, public modalCtrl: ModalController) {
+  isComment = false;
+  isAnswer = false;
+  isActive = false;
+  hideQuestionBtn: boolean = true;
+  showToastr: boolean = false;
+  className;
+  msgTitle;
+  msgDes;
+  constructor(public navCtrl: NavController, public loader: LoaderService, public navParams: NavParams, private modalService: BsModalService, public constant: Constant, private toastr: ToastrService, public API_URL: API_URL, private http: HttpProvider, private storage: Storage, public modalCtrl: ModalController) {
     this.forumDetailObject = this.navParams.data;
     //this.employees = this.forumDetailObject['setData']['employees'];
     this.topics = this.forumDetailObject['setData']['topics'];
@@ -67,9 +71,9 @@ export class ForumDetailPage implements OnInit {
     this.forumTopics = 'mostRecent';
   }
   ionViewDidEnter() {
-    this.status='recent';
+    this.status = 'recent';
     this.getTopicsRelated();
-    
+
   }
 
   calculateExpireDays(dueDate) {
@@ -88,124 +92,140 @@ export class ForumDetailPage implements OnInit {
   }
   getTopicsRelated() {
     this.loader.showLoader();
-    this.http.get(API_URL.URLS.post+'?forumId='+this.forumId+'&status='+this.status).subscribe((res) => {
+    this.http.get(API_URL.URLS.post + '?forumId=' + this.forumId + '&status=' + this.status).subscribe((res) => {
       if (res['isSuccess']) {
         this.recentList = res['data'];
-      }else{
-        this.recentList=[];
+      } else {
+        this.recentList = [];
       }
       this.loader.hideLoader();
     });
   }
-  
+
   modalRef: BsModalRef;
- 
+
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
-  openEditModal(template: TemplateRef<any>, comment){
-       this.editComment = comment.description;
-       this.commentId = comment.commentId;
-       this.postId= comment.postId;
-       this.description = comment.description;
-       this.createdBy =comment.createdBy;
-       this.modalRef = this.modalService.show(template);
+  openEditModal(template: TemplateRef<any>, comment) {
+    this.editComment = comment.description;
+    this.commentId = comment.commentId;
+    this.postId = comment.postId;
+    this.description = comment.description;
+    this.createdBy = comment.createdBy;
+    this.modalRef = this.modalService.show(template);
   }
 
-  changeEditInput(){
+  changeEditInput() {
     this.editDisable = false;
   }
 
-  updateComment(){
+  updateComment() {
     let postData = {
-       'description' : this.editComment,
-       'postId': this.postId,
-       'createdBy':this.createdBy
+      'description': this.editComment,
+      'postId': this.postId,
+      'createdBy': this.createdBy
     }
-    this.http.put(false,API_URL.URLS.comment+'/'+ this.commentId,postData).subscribe(res=>{
-      if(res['isSuccess']){
-        this.toastr.success(res['data']);
+    this.http.put(false, API_URL.URLS.comment + '/' + this.commentId, postData).subscribe(res => {
+      if (res['isSuccess']) {
+        this.successMessage(res['data']);
+        //this.toastr.success(res['data']);
         this.modalRef.hide();
         this.getTopicsRelated();
       }
     })
   }
-  
+
   //Add post
   questionSubmit() {
-    let postData={
-      "description" : this.forum.question,
+    let postData = {
+      "description": this.forum.question,
       "forumId": this.forumId,
       "createdBy": this.currentUser.userId,
       "votesCount": 0
     }
     this.loader.showLoader();
-    this.http.post(false,API_URL.URLS.post,postData).subscribe(res=>{
-       if(res['isSuccess']){
-         this.toastr.success(res['data']);
-          this.getTopicsRelated();
-          this.forum.question = '';
-          this.modalRef.hide();
-       }
-     this.loader.hideLoader();  
+    this.http.post(false, API_URL.URLS.post, postData).subscribe(res => {
+      if (res['isSuccess']) {
+         this.successMessage(res['data']);
+        // this.toastr.success(res['data']);
+        this.getTopicsRelated();
+        this.forum.question = '';
+        this.modalRef.hide();
+      }
+      this.loader.hideLoader();
     })
-    
+
+  }
+  successMessage(msg){
+
+    this.showToastr = true;
+    this.className = "notify-box alert alert-success";
+    this.msgTitle = "Success";
+    this.msgDes = msg ;
+    let self = this;
+    setTimeout(function(){ 
+      self.showToastr = false;
+      }, 3000); 
+
   }
 
- //change status between recent and fav.
-  changeStatus(status){
+  //change status between recent and fav.
+  changeStatus(status) {
     this.status = status;
     this.getTopicsRelated();
   }
 
   //Make a post fav/unfav
   likeUnlikeQuestion(list, status) {
-    let postData={
+    let postData = {
       'isFavorite': status
     }
     this.loader.showLoader();
-    this.http.put(false,API_URL.URLS.post+'/'+list.postId,postData).subscribe((res) => {
+    this.http.put(false, API_URL.URLS.post + '/' + list.postId, postData).subscribe((res) => {
       if (res['isSuccess']) {
-        this.toastr.success(res['data']);
+      //  this.toastr.success(res['data']);
+
+      this.successMessage(res['data']);
         this.getTopicsRelated();
       }
       this.loader.hideLoader();
     });
   }
 
- //hide and show description part
+  //hide and show description part
   hideShowDesc(list, j) {
     this.hideQuestionBtn = true;
     list['isActive'] = !(list['isActive']);
-    list['isComment'] = list['isComment'] ? false :'';
-    list['isAnswer'] = list['isAnswer'] ? false :'';
+    list['isComment'] = list['isComment'] ? false : '';
+    list['isAnswer'] = list['isAnswer'] ? false : '';
   }
 
 
-  showAnswers(list){
+  showAnswers(list) {
     this.getComments(list['postId']);
     list['isAnswer'] = !list['isAnswer'];
-    list['isComment'] = list['isComment'] ? false :'';
+    list['isComment'] = list['isComment'] ? false : '';
 
   }
 
   //hide and show comment part
   showCommentSet(list, j) {
     list['isComment'] = !(list['isComment']);
-    list['isAnswer'] = list['isAnswer'] ? false :'';
-    this.hideQuestionBtn = false ;
+    list['isAnswer'] = list['isAnswer'] ? false : '';
+    this.hideQuestionBtn = false;
   }
-  
+
   //Get comments by postId
   getComments(id) {
-    this.http.get(API_URL.URLS.comment+'/'+id).subscribe((res) => {
+    this.http.get(API_URL.URLS.comment + '/' + id).subscribe((res) => {
       if (res['isSuccess']) {
         this.commentList = res['data']['rows'];
       }
     });
   }
-  
+
   getUser() {
     this.storage.get('currentUser').then(
       (val) => {
@@ -218,51 +238,52 @@ export class ForumDetailPage implements OnInit {
       });
   }
 
-  saveComment(id,i) {  
-   let postData ={
-     "description": this.comment,
-     "createdBy" : this.currentUser.userId,
-     "postId": id
-   }
-     if (!this.comment) {
+  saveComment(id, i) {
+    let postData = {
+      "description": this.comment,
+      "createdBy": this.currentUser.userId,
+      "postId": id
+    }
+    if (!this.comment) {
       this.toastr.error("Comment is required");
     } else {
-      this.http.post(false,API_URL.URLS.comment,postData).subscribe(res=>{
-        if(res['isSuccess']){
+      this.http.post(false, API_URL.URLS.comment, postData).subscribe(res => {
+        if (res['isSuccess']) {
           this.getTopicsRelated();
           this.getComments(id);
-          this.toastr.success(res['data']);
-           this.comment = '';
+         // this.toastr.success(res['data']);
+         this.successMessage(res['data']);
+          this.comment = '';
         }
-      })  
+      })
     }
   }
 
-  likePost(list,status){
-    let postData={};
-    
-    if(status == 'like'){
+  likePost(list, status) {
+    let postData = {};
+
+    if (status == 'like') {
       list['like'] = true;
       postData['votesCount'] = list.votesCount + 1;
     }
 
-    this.http.put(false,API_URL.URLS.post+'/'+list.postId, postData).subscribe(res=>{
-      if(res['isSuccess']){
+    this.http.put(false, API_URL.URLS.post + '/' + list.postId, postData).subscribe(res => {
+      if (res['isSuccess']) {
         this.getTopicsRelated();
       }
     })
   }
 
-  deleteComment(){
-    this.http.delete(API_URL.URLS.comment+'/'+this.commentId).subscribe(res=>{
-      if(res['isSuccess']){
-        this.toastr.success(res['data']);
+  deleteComment() {
+    this.http.delete(API_URL.URLS.comment + '/' + this.commentId).subscribe(res => {
+      if (res['isSuccess']) {
+        this.successMessage(res['data']);
+      //  this.toastr.success(res['data']);
         this.modalRef.hide();
         this.getTopicsRelated();
       }
     })
   }
-
   async presentModal(repliesObj) {
     const modal = await this.modalCtrl.create({
       component: ModalPage,
@@ -270,7 +291,7 @@ export class ForumDetailPage implements OnInit {
     });
     return await modal.present();
   }
-  goBackLevel(){
+  goBackLevel() {
     this.navCtrl.setRoot('forum-page');
   }
 }
