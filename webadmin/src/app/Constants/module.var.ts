abff4c75f83405ba614dd6e7918eb127983cc9b7
gisvignesh <vignesh.k@greatinnovus.com>
2019-07-04 15:54:57 +0530

    RL back button issue-fixed

--- webadmin/src/app/Constants/module.var.ts
+++ webadmin/src/app/Constants/module.var.ts
@@ -18,7 +18,7 @@ export class ModuleVar {
     videoLink;
     selectedModule;
     selectedVideo=[];
-    selectedCourse;
+    selectedCourse=[];
     courseVideos=[];
     selectedCourses = null;
     selectedTrainingClass = null;
