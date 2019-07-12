c064b045f4e9f37271dddc4282baf9631a254245
GisBooshna <booshna.p@greatinnovus.com>
2019-07-12 15:12:03 +0530

    socket service

--- webadmin/src/app/Constants/api.ts
+++ webadmin/src/app/Constants/api.ts
@@ -4,7 +4,11 @@ import { Injectable } from '@angular/core';
 
 export class API {
     public static API_URL ='https://www.mocky.io/v2/';
-    public static API_ENDPOINT = 'http://demo.greatinnovus.com:'; 
+    //public static API_ENDPOINT = 'http://demo.greatinnovus.com:'; 
+    public static API_ENDPOINT = 'http://192.168.1.41:'; 
+    public static SOCKET_PORT = 8181;
+
+
 }
 
 
