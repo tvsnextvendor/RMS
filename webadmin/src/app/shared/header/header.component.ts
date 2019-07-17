0c9d6fd8f20f34857727d1ecd505c6739091e459
GisBooshna <booshna.p@greatinnovus.com>
2019-07-17 09:12:50 +0530

    notification

--- webadmin/src/app/shared/header/header.component.ts
+++ webadmin/src/app/shared/header/header.component.ts
@@ -8,6 +8,7 @@ import { Header } from '../../Constants/header';
 import { HeaderVar } from 'src/app/Constants/header.var';
 import {SocketService} from '../socket.service';
 import { API_URL } from '../../Constants/api_url';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-header',
@@ -18,6 +19,9 @@ export class HeaderComponent implements OnInit {
   headerData: Header = {} as any;
   filtersLoaded: Promise<boolean>;
   breadCrumbData;
+  uploadPath;
+  notificationList;
+  notificationCount;
 
   constructor(public commonService:CommonService,public socketService: SocketService,public utilService: UtilService,public headerVar: HeaderVar,private headerService: HeaderService,private breadCrumbService : BreadCrumbService,private http: HttpService,public router:Router,public authGuard:AuthGuard) { 
     const currentUrl = this.router.url;
@@ -26,16 +30,15 @@ export class HeaderComponent implements OnInit {
   }
    
   ngOnInit(){
-   // this.getNotification();
+    let userData= this.utilService.getUserData();
+    console.log(userData, "USERDATA");
+    this.uploadPath = userData ? userData.uploadPaths.uploadPath : '';
+    this.getNotification();
     this.headerService.TitleDetail.subscribe((resp) => { 
       setTimeout(() =>{ this.headerVar.title=resp.title,
           this.headerVar.hideModule=resp.hidemodule 
         })
     });
-    
-    this.http.get(this.headerVar.url.getNotifications).subscribe((data) => {
-       this.headerVar.notificationList = data;
-    })
   }
 
   ngDoCheck(){
@@ -52,18 +55,32 @@ export class HeaderComponent implements OnInit {
   }
 
   getNotification(){
-    let userData = this.utilService.getUserData();
-    //console.log(userData.userId)
+      let userData = this.utilService.getUserData();
       let socketObj = {
         userId : userData.userId
       };
-      setInterval(() => {
+      this.socketService.getNotification(socketObj).subscribe((data) => {
+          this.notificationCount = data['unReadCount'];
+          this.notificationList = data['rows'];
+      });
+       setInterval(() => {
          this.socketService.getNotification(socketObj).subscribe((data) => {
-             //console.log(data)
-             //this.notificationCount = data['unReadCount'];
+             console.log(data)
+             this.notificationCount = data['unReadCount'];
+             this.notificationList = data['rows'];
          });
-      }, 10000);
-     
+       }, 15000);  
    }
+
+   calculateHours(date){
+    var a = moment(new Date(date));
+    var b = moment(new Date());
+    let day =  a.from(b, true) // "2 days ago"
+    var temp = day.split(" ")//now you have 3 words in temp
+    if(temp[0] == 'a' || temp[0] == 'an'){
+      temp[0] = '1';
+    }
+    return temp[0] + temp[1].charAt(0); // return as 2d
+  }
 
 }
