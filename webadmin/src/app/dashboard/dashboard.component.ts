a0b53c9dfb3cfdddb9a31fc3e8c36bffb0b53195
GISDeepika <deepika.b@greatinnovus.com>
2019-07-22 12:03:21 +0530

    dashboard notification count update

--- webadmin/src/app/dashboard/dashboard.component.ts
+++ webadmin/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DashboardVar} from '../Constants/dashboard.var';
 import {HeaderService,BreadCrumbService} from '../services';
+import {SocketService} from '../shared/socket.service';
 import {UtilService} from '../services/util.service';
 import { CommonLabels } from '../Constants/common-labels.var'
 
@@ -13,13 +14,15 @@ export class DashboardComponent implements OnInit {
   tabs = [];
   
   tabTitle = [];
+  notificationCount = 0;
   selectedtab;
 
-  constructor(private dashboardVar: DashboardVar,private utilService: UtilService ,private headerService: HeaderService,public commonLabels:CommonLabels,private breadCrumbService :BreadCrumbService) { }
+  constructor(private dashboardVar: DashboardVar,private utilService: UtilService ,private socketService:SocketService,private headerService: HeaderService,public commonLabels:CommonLabels,private breadCrumbService :BreadCrumbService) { }
 
   ngOnInit() {
   this.headerService.setTitle({title:this.commonLabels.titles.dashboard, hidemodule:false});
-  this.breadCrumbService.setTitle([])
+  this.breadCrumbService.setTitle([]);
+  this.getNotification();
   const roleId = this.utilService.getRole();
   let user = this.utilService.getUserData();
   let parentId = user && user.ResortUserMappings.length && user.ResortUserMappings[0].Resort && user.ResortUserMappings[0].Resort.parentId ? user.ResortUserMappings[0].Resort.parentId : '';
@@ -42,6 +45,25 @@ export class DashboardComponent implements OnInit {
       // this.selectTab(this.tabs.first);
     }
   }
+
+  getNotification(){
+    let userData = this.utilService.getUserData();
+    let socketObj = {
+      userId : userData.userId
+    };
+    this.socketService.getNotification(socketObj).subscribe((data) => {
+        this.notificationCount = data['unReadCount'];
+        // this.notificationList = data['rows'];
+    });
+     setInterval(() => {
+       this.socketService.getNotification(socketObj).subscribe((data) => {
+           this.notificationCount = data['unReadCount'];
+          //  this.notificationList = data['rows'];
+          //  console.log(this.notificationList)
+
+       });
+     }, 15000);  
+ }
 
   
   selectTab(tab: any) {
