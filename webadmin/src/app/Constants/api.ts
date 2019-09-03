960b4d6ef966392da155da407dd81aa86faa0a70
gisvignesh <vignesh.k@greatinnovus.com>
2019-09-03 13:12:32 +0530

    label update

--- webadmin/src/app/Constants/api.ts
+++ webadmin/src/app/Constants/api.ts
@@ -6,7 +6,7 @@ export class API {
     public static API_URL ='https://www.mocky.io/v2/';
     public static API_ENDPOINT = 'http://demo.greatinnovus.com:'; 
     // public static API_ENDPOINT = 'http://192.168.1.21:'; 
-    public static AWS = true;
+    public static AWS = false;
     public static SOCKET_PORT = 8181;
 }
 
