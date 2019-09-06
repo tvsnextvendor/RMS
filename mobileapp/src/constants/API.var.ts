d34058327ed270b740249dfcc22aff1d25aa76e6
GisBooshna <booshna.p@greatinnovus.com>
2019-09-06 18:39:59 +0530

    constant

--- mobileapp/src/constants/API.var.ts
+++ mobileapp/src/constants/API.var.ts
@@ -1,7 +1,7 @@
 export class API {
     public static API_URL ='https://www.mocky.io/v2/'; 
     public static API_LINK = 'http://demo.greatinnovus.com:';
-    public static APP_VER = 40;
+    public static APP_VER = 43;
     //public static API_LINK = 'http://192.168.1.41:'; 
     public static SOCKET_PORT = 8181;
 } 
