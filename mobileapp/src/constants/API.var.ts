6ff471b63414bc43218ae7e17c39cddd95386600
sheema <sheema.n@greatinnovus.com>
2019-10-02 10:05:22 +0530

    commit code updated

--- mobileapp/src/constants/API.var.ts
+++ mobileapp/src/constants/API.var.ts
@@ -1,7 +1,7 @@
 export class API {
     public static API_URL ='https://www.mocky.io/v2/'; 
     //public static API_LINK = 'http://demo.greatinnovus.com:';
-    public static APP_VER = 43;
+    public static APP_VER = 51;
     public static API_LINK = 'http://ec2-34-227-100-123.compute-1.amazonaws.com:'; 
     public static SOCKET_PORT = 8181;
 } 
