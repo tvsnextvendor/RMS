6bccd05b562016016bc710d3c8f783182b7ced68
sheema <sheema.n@greatinnovus.com>
2019-10-02 08:23:53 +0530

    commit code updated for staging server API link

--- mobileapp/src/constants/API.var.ts
+++ mobileapp/src/constants/API.var.ts
@@ -1,8 +1,8 @@
 export class API {
     public static API_URL ='https://www.mocky.io/v2/'; 
-    public static API_LINK = 'http://demo.greatinnovus.com:';
+    //public static API_LINK = 'http://demo.greatinnovus.com:';
     public static APP_VER = 43;
-    //public static API_LINK = 'http://192.168.1.41:'; 
+    public static API_LINK = 'http://ec2-34-227-100-123.compute-1.amazonaws.com:'; 
     public static SOCKET_PORT = 8181;
 } 
 
